4b00db96973ed0546ebdf332976f3cf4c153463b
spookyexe <gianepanto@gmail.com>
2025-02-22 13:48:54 +0800

    FEAT: Integrate react-hook-form for LogInDialog; Implement form handling and validation

--- src/app/components/LogInDialog.tsx
+++ src/app/components/LogInDialog.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import React from "react";
-import Link from "next/link";
-
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
 import {
   DialogContent,
   DialogDescription,
@@ -10,27 +10,57 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 const LogInDialog = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const router = useRouter();
+
+  const onSubmit = () => {
+    // Log in logic here
+    router.push("/id");
+  };
+
   return (
-    <>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Log In</DialogTitle>
-          <DialogDescription>
-            Include Login Form Here in the future.
-          </DialogDescription>
-        </DialogHeader>
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Log In</DialogTitle>
+        <DialogDescription>Enter your credentials to log in.</DialogDescription>
+      </DialogHeader>
+
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+        <div className="space-y-2">
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            type="email"
+            placeholder="you@example.com"
+            {...register("email", { required: true })}
+          />
+        </div>
+
+        <div className="space-y-2">
+          <Label htmlFor="password">Password</Label>
+          <Input
+            id="password"
+            type="password"
+            placeholder="••••••••"
+            {...register("email", { required: true })}
+          />
+        </div>
 
         <DialogFooter>
-          <Button asChild>
-            <Link href="/id">Log In</Link>
-          </Button>
+          <Button type="submit">Log In</Button>
         </DialogFooter>
-      </DialogContent>
-    </>
+      </form>
+    </DialogContent>
   );
 };
 
