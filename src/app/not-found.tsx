40e457c8b93c5157d79fb16fe679030018101e97
spookyexe <gianepanto@gmail.com>
2025-02-22 23:43:37 +0800

    FEAT: add custom 404 error page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Link from "next/link";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+export default function Profile() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center p-8">
+      <Card className="min-w-80 h-auto">
+        <CardHeader>
+          <Avatar>
+            <AvatarImage src="../logo.png" />
+            <AvatarFallback>logo</AvatarFallback>
+          </Avatar>
+
+          <CardTitle className="font-primary font-extrabold">
+            DOST SA USC
+          </CardTitle>
+          <CardDescription>
+            DOST Scholars’ Association in the University of San Carlos.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-2">
+          <h1 className="text-center">
+            <span className="text-destructive font-bold">Error 404</span>. Page
+            Not Found
+          </h1>
+          <Button className="w-full" variant="outline" asChild>
+            <Link href="/">Go back to Home</Link>
+          </Button>
+        </CardContent>
+        <CardFooter className="text-sm">
+          <p>© 2025, DOST SA USC. All Rights Reserved.</p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
